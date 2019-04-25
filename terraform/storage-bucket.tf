dcdad316fd149dbc68b34dd7ac60eafaab792a54
Sergey Ovcharenko <sv@sv-it.ru>
2019-04-25 23:07:57 +0200

    Add terraform storage-bucket

---
+++ terraform/storage-bucket.tf
@@ -0,0 +1,15 @@
+provider "google" {
+version = "2.0.0"
+project = "${var.project}"
+region = "${var.region}"
+}
+module "storage-bucket" {
+source = "SweetOps/storage-bucket/google"
+version = "0.1.1"
+# Имена
+name = ["storage-bucket-test-235108", "storage-bucket-test2-235108"]
+}
+output storage-bucket_url {
+value = "${module.storage-bucket.url}"
+}
+
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable project {
+  description = "Project ID"
+}
+
+variable region {
+  description = "Region"
+
+  # Значение по умолчанию
+  default = "europe-west1"
+}
+
