669c04f8ddf503b1a6cc96788b207591c796b2d0
Sergey Ovcharenko <sv@sv-it.ru>
2019-04-25 14:37:56 +0200

    Add variable private_key_path

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,6 +4,7 @@ variable project {
 
 variable region {
   description = "Region"
+
   # Значение по умолчанию
   default = "europe-west1"
 }
@@ -13,12 +14,18 @@ variable public_key_path {
   description = "Path to the public key used for ssh access"
 }
 
+variable private_key_path {
+  # Описание переменной
+  description = "Path to the private key used for ssh access"
+}
+
 variable disk_image {
   description = "Disk image"
 }
 
 variable zone {
   description = "zone"
+
   # Значение по умолчанию
   default = "europe-west1-b"
 }
