acf2b8f62f7c224e6895d5270fedd998a42a7654
Sergey Ovcharenko <sv@sv-it.ru>
2019-04-25 15:20:47 +0200

    Add resource firewall_ssh

--- terraform/main.tf
+++ terraform/main.tf
@@ -77,3 +77,16 @@ resource "google_compute_firewall" "firewall_puma" {
   # Правило применимо для инстансов с перечисленными тэгами
   target_tags = ["reddit-app"]
 }
+
+resource "google_compute_firewall" "firewall_ssh" {
+  name    = "default-allow-ssh"
+  network = "default"
+  description = "Allow SSH from anywhere"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
